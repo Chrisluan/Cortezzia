cb82518c9e0936e9b97e4ef7823d6ac9ad8b849e
chrisluan <chrisluan.dev@gmail.com>
2025-02-27 13:00:31 -0300

    `feat: updated log-in form to fetch logged user data from backend`

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import {
 import { FaLock, FaMailBulk } from "react-icons/fa";
 
 function App() {
+  const [loggedUser, setLoggedUser] = useState();
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -26,89 +27,101 @@ function App() {
     console.log("Dados enviados:", formData);
 
     const queryString = new URLSearchParams(formData).toString();
-    const response = await fetch(`http://localhost:3000/log-in?${queryString}`, {
-      method: "GET",
-    });
+    const response = await fetch(
+      `http://localhost:3000/log-in?${queryString}`,
+      {
+        method: "GET",
+      }
+    );
 
-    if(!response.ok){
-
+    if (!response.ok) {
+      console.log("Erro ao buscar usuario");
+      return;
     }
-    console.log(await response.json())
+    setLoggedUser(await response.json());
     // Aqui você pode manipular os dados, como enviá-los para um backend
   };
-
-  return (
-    <Flex justifyContent={"center"}>
-      <Flex
-        maxWidth={"80%"}
-        justifyContent={"center"}
-        alignItems={"center"}
-        height={"100dvh"}
-        width={"500px"}
-      >
-        <FormControl
-          sx={{
-            textAlign: "center",
-            border: "1px solid black",
-            height: "300px",
-            padding: "20px",
-            maxW: "400px",
-          }}
-        >
-          <Text
-            sx={{
-              fontSize: "x-large",
-              fontWeight: "600",
-            }}
-          >
-            Log-in
-          </Text>
+  if (!loggedUser) {
+    return (
+      <>
+        <Flex justifyContent={"center"}>
           <Flex
-            sx={{
-              gap: "10px",
-            }}
-            height={"fit-content"}
-            flexDir={"column"}
+            maxWidth={"80%"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            height={"100dvh"}
+            width={"500px"}
           >
-            <InputGroup>
-              <InputAddon>
-                <FaMailBulk />
-              </InputAddon>
-              <Input
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                placeholder="Email"
-              />
-            </InputGroup>
-            <InputGroup>
-              <InputAddon>
-                <FaLock />
-              </InputAddon>
-              <Input
-                name="password"
-                type="password"
-                value={formData.password}
-                onChange={handleChange}
-                placeholder="Senha"
-              />
-            </InputGroup>
-            <Button
+            <FormControl
               sx={{
-                width: "100%",
+                textAlign: "center",
+                border: "1px solid black",
+                height: "300px",
                 padding: "20px",
-                fontSize: "x-large",
-              }}
-              colorScheme="blue"
-              onClick={async ()=>{
-                await handleSubmit();
+                maxW: "400px",
               }}
             >
-              Log-in
-            </Button>
+              <Text
+                sx={{
+                  fontSize: "x-large",
+                  fontWeight: "600",
+                }}
+              >
+                Log-in
+              </Text>
+              <Flex
+                sx={{
+                  gap: "10px",
+                }}
+                height={"fit-content"}
+                flexDir={"column"}
+              >
+                <InputGroup>
+                  <InputAddon>
+                    <FaMailBulk />
+                  </InputAddon>
+                  <Input
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    placeholder="Email"
+                  />
+                </InputGroup>
+                <InputGroup>
+                  <InputAddon>
+                    <FaLock />
+                  </InputAddon>
+                  <Input
+                    name="password"
+                    type="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    placeholder="Senha"
+                  />
+                </InputGroup>
+                <Button
+                  sx={{
+                    width: "100%",
+                    padding: "20px",
+                    fontSize: "x-large",
+                  }}
+                  colorScheme="blue"
+                  onClick={async () => {
+                    await handleSubmit();
+                  }}
+                >
+                  Log-in
+                </Button>
+              </Flex>
+            </FormControl>
           </Flex>
-        </FormControl>
-      </Flex>
+        </Flex>
+      </>
+    );
+  }
+  return (
+    <Flex>
+      <Text>Usuario logado na barbearia {loggedUser[0].barbearia.nome}</Text>
     </Flex>
   );
 }
